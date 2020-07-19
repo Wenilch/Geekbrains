b43e73e84eb203b7eabf83a89f4e99349893cc9b
Vlad Panteleev <vlad.panteleev@hotmail.com>
2020-07-19 21:46:40 +0400

    Реализовано домашнее задание №7.

--- Java/chat/chat-server/src/main/java/ru/geekbrains/core/ChatServer.java
+++ Java/chat/chat-server/src/main/java/ru/geekbrains/core/ChatServer.java
@@ -1,20 +1,17 @@
 package ru.geekbrains.core;
 
-import ru.geekbrains.chat.common.MessageLibrary;
-import ru.geekbrains.net.MessageSocketThreadListener;
 import ru.geekbrains.net.ServerSocketThread;
 import ru.geekbrains.net.ServerSocketThreadListener;
 
 import java.net.Socket;
 import java.util.Vector;
 
-public class ChatServer implements ServerSocketThreadListener, MessageSocketThreadListener {
+public class ChatServer implements ServerSocketThreadListener {
 
     private ServerSocketThread serverSocketThread;
-    private ClientSessionThread clientSession;
     private ChatServerListener listener;
     private AuthController authController;
-    private Vector<ClientSessionThread> clients = new Vector<>();
+    protected Vector<ClientSessionThread> clients = new Vector<>();
 
     public ChatServer(ChatServerListener listener) {
         this.listener = listener;
@@ -24,7 +21,7 @@ public class ChatServer implements ServerSocketThreadListener, MessageSocketThre
         if (serverSocketThread != null && serverSocketThread.isAlive()) {
             return;
         }
-        serverSocketThread = new ServerSocketThread(this,"Chat-Server-Socket-Thread", port, 2000);
+        serverSocketThread = new ServerSocketThread(this, "Chat-Server-Socket-Thread", port, 2000);
         serverSocketThread.start();
         authController = new AuthController();
         authController.init();
@@ -44,7 +41,11 @@ public class ChatServer implements ServerSocketThreadListener, MessageSocketThre
 
     @Override
     public void onSocketAccepted(Socket socket) {
-        this.clientSession = new ClientSessionThread(this, "ClientSessionThread", socket);
+        ClientMessageSocketThreadListener clientMessageSocketThreadListener = new ClientMessageSocketThreadListener(this, authController);
+        ClientSessionThread clientSessionThread = new ClientSessionThread(clientMessageSocketThreadListener, "ClientSessionThread", socket);
+        clientMessageSocketThreadListener.setClientSession(clientSessionThread);
+
+        clients.add(clientSessionThread);
     }
 
     @Override
@@ -57,54 +58,10 @@ public class ChatServer implements ServerSocketThreadListener, MessageSocketThre
 
     }
 
-    @Override
-    public void onSocketReady() {
-        logMessage("Socket ready");
-    }
-
-    @Override
-    public void onSocketClosed() {
-        logMessage("Socket Closed");
-    }
-
-    @Override
-    public void onMessageReceived(String msg) {
-        if (clientSession.isAuthorized()) {
-            processAuthorizedUserMessage(msg);
-        } else {
-            processUnauthorizedUserMessage(msg);
-        }
-
-
-    }
-
-    private void processAuthorizedUserMessage(String msg) {
-        logMessage(msg);
-        clientSession.sendMessage("echo: " + msg);
-    }
-
-    private void processUnauthorizedUserMessage(String msg) {
-        String[] arr = msg.split(MessageLibrary.DELIMITER);
-        if (arr.length < 4 ||
-                !arr[0].equals(MessageLibrary.AUTH_METHOD) ||
-                !arr[1].equals(MessageLibrary.AUTH_REQUEST)) {
-            clientSession.authError("Incorrect request: " + msg);
-            return;
-        }
-        String login = arr[2];
-        String password = arr[3];
-        String nickname = authController.getNickname(login, password);
-        if (nickname == null) {
-            clientSession.authDeny();
-            return;
-        }
-        clientSession.authAccept(nickname);
-     }
-
     public void disconnectAll() {
     }
 
-    private void logMessage(String msg) {
+    protected void logMessage(String msg) {
         listener.onChatServerMessage(msg);
     }
 }
